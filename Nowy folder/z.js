c1c229e16e3baf59d451e243c5389fedea3bd458
Drym <106377232+Drym1@users.noreply.github.com>
2024-09-11 11:36:52 +0200

    Add files via upload

---
+++ Nowy folder/z.js
@@ -0,0 +1,28 @@
+console.log("witaj Świecie");
+
+let tekst = "Witaj świecie!";
+console.log(tekst);
+
+let e = 2.718228;
+alert(e);
+
+
+let zmienna = 12;
+console.log(typeof zmienna);
+
+zmienna = "Język Java Script";
+console.log(typeof zmienna);
+
+
+let opis = "JavaScript to język programowania.";
+console.log("informacja" + opis);
+
+let a = 21;
+let b = 17;
+
+alert(a + "+" + b + "=" + (a+b));
+
+
+let c = prompt("podaj liczbę");
+console.log("pobrna liczba" + c);
+
