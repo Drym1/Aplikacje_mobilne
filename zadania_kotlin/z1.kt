09928fee585b7acda732735ed8595d3c7d0fed41
Drym <106377232+Drym1@users.noreply.github.com>
2024-09-12 14:25:50 +0200

    Add files via upload

---
+++ zadania_kotlin/z1.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication3
+
+fun main(){
+    var tekst = readLine()
+
+    println(tekst)
+
+}
+
+
+fun funkcja(){
+
+    var va3 = readLine()
+
+    when(va3){
+        "pomidor" -> println("pomicor")
+        "truskawka" -> println("truskawka")
+        else -> print("to ani truskawka ani pomidor")
+
+    }
+
+
+
+
+}
